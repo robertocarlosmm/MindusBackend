f94472f1c32591e64d23e2b20fdc49d129c9b50c
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2025-07-11 18:39:40 -0500

    fix it

--- src/links/product-productoextension.ts
+++ src/links/product-productoextension.ts
@@ -3,6 +3,6 @@ import { defineLink } from "@medusajs/framework/utils"
 import productoExtension from "../modules/productoExtension";
 
 export default defineLink(
-    ProductModule.linkable.store,
+    ProductModule.linkable.product,
     productoExtension.linkable.productoExtension
 );
